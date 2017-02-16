a28e37c4e41122d7ee8e72a4212bfc8e1b33fab0
Rendoir <Rendoir@172.30.12.47>
2017-02-16 15:03:46 +0000

    Added map and print function

--- TP1/src/DungeonKeep.java
+++ TP1/src/DungeonKeep.java
@@ -1,7 +1,31 @@
 public class DungeonKeep {
 
+	public static void printMap(char[][] map){
+		for(int i = 0; i < map.length; i++){
+			for(int j = 0; j < map[i].length; j++){
+				System.out.print(map[i][j]);
+				System.out.print(' ');
+			}
+			System.out.println();
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		
+		char map[][] = {
+				{'X','X','X','X','X','X','X','X','X','X',}, 
+				{'X','H',' ',' ','I',' ','X',' ','G','X',},
+				{'X','X','X',' ','X','X','X',' ',' ','X',},
+				{'X',' ','I',' ','I',' ','X',' ',' ','X',},
+				{'X','X','X',' ','X','X','X',' ',' ','X',},
+				{'I',' ',' ',' ',' ',' ',' ',' ',' ','X',},
+				{'I',' ',' ',' ',' ',' ',' ',' ',' ','X',},
+				{'X','X','X',' ','X','X','X','X',' ','X',},
+				{'X',' ','I',' ','I',' ','X','k',' ','X',},
+				{'X','X','X','X','X','X','X','X','X','X',}};
+		
+		printMap(map);
 	}
 
 }
