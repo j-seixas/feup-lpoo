6db2ba0fea7719a75d0b1b13484173b19f1696cc
Rendoir <Rendoir@DESKTOP-NDVIBIV.lan>
2017-02-25 15:36:10 +0000

    Fixed the key bug. Everything works!

--- TP1/src/dk/logic/Ogre.java
+++ TP1/src/dk/logic/Ogre.java
@@ -2,81 +2,77 @@ package dk.logic;
 
 import java.util.Random;
 
-
 public class Ogre extends Character {
 
 	private boolean ogre_on_key = false;
-	
-	
+
 	public Ogre(int x, int y) {
-		super(x,y);
+		super(x, y);
 	}
-	
+
 	public boolean moveCharacter(Game game) {
 		boolean can_move, on_key = false;
 		char draw_char = '\0';
 		int ogreDirection;
 		do {
-		can_move = true;
-		Random rand = new Random();
-		ogreDirection = rand.nextInt(4);
-		boolean insideCanvas = false;
-		char nextCharacter = '\0';
-		switch (ogreDirection) {
-		case 0:
-			if (getY() > 0) {
-				insideCanvas = true;
-				nextCharacter = game.getMap(getY() - 1,getX());
-			}
-			break;
-		case 1:
-			if (getY() < 8) {
-				insideCanvas = true;
-				nextCharacter = game.getMap(getY() + 1,getX());
-			}
-			break;
-		case 2:
-			if (getX() < 8) {
-				insideCanvas = true;
-				nextCharacter = game.getMap(getY(),getX() + 1);
+			can_move = true;
+			Random rand = new Random();
+			ogreDirection = rand.nextInt(4);
+			boolean insideCanvas = false;
+			char nextCharacter = '\0';
+			switch (ogreDirection) {
+			case 0:
+				if (getY() > 0) {
+					insideCanvas = true;
+					nextCharacter = game.getMap(getX() ,getY() - 1 );
+				}
+				break;
+			case 1:
+				if (getY() < 8) {
+					insideCanvas = true;
+					nextCharacter = game.getMap(getX() ,getY() + 1 );
+				}
+				break;
+			case 2:
+				if (getX() < 8) {
+					insideCanvas = true;
+					nextCharacter = game.getMap(getX() + 1,getY());
 
+				}
+				break;
+			case 3:
+				if (getX() > 0) {
+					insideCanvas = true;
+					nextCharacter = game.getMap(getX() - 1,getY());
+				}
+				break;
 			}
-			break;
-		case 3:
-			if (getX() > 0) {
-				insideCanvas = true;
-				nextCharacter = game.getMap(getY(),getX() - 1);
-			}
-			break;
-		}
-		if (insideCanvas) {
-			if (nextCharacter == ' ' || nextCharacter == '*')
-				draw_char = 'O';
-			else if(nextCharacter == 'k'){
-				draw_char = '$';
-				on_key = true;
-			}
-			else can_move = false; 
-		}
-		else can_move = false;
-		}while(!can_move);
-		
-		
+			if (insideCanvas) {
+				if (nextCharacter == ' ' || nextCharacter == '*')
+					draw_char = 'O';
+				else if (nextCharacter == 'k') {
+					draw_char = '$';
+					on_key = true;
+				} else
+					can_move = false;
+			} else
+				can_move = false;
+		} while (!can_move);
+
 		moveOgre(ogreDirection, draw_char, game);
-		
-		if(on_key)
+
+		if (on_key)
 			ogre_on_key = true;
-		
+
 		return true;
 	}
 
 	private void moveOgre(int direction, char draw_char, Game game) {
-		if(ogre_on_key){
-			game.setMap(coordinates,'k');
+		if (ogre_on_key) {
+			game.setMap(coordinates, 'k');
 			ogre_on_key = false;
-		}
-		else game.setMap(coordinates,' ');
-
+		} else
+			game.setMap(coordinates, ' ');
 
 		switch (direction) {
 		case 0:
@@ -93,7 +89,7 @@ public class Ogre extends Character {
 			break;
 		}
 
-		game.setMap(coordinates,draw_char);
+		game.setMap(coordinates, draw_char);
 	}
-	
+
 }
