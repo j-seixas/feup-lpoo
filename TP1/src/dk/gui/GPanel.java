0fb58749bdad604c6e68777e0c21e6820a64d617
João Seixas <up201505648@fe.up.pt>
2017-03-18 18:40:17 +0000

    Added Images for Ogre, Walls, Guardian and Hero

--- TP1/src/dk/gui/GPanel.java
+++ TP1/src/dk/gui/GPanel.java
@@ -2,6 +2,11 @@ package dk.gui;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class GPanel extends JPanel {
@@ -36,13 +41,15 @@ public class GPanel extends JPanel {
 			return Color.GREEN;
 		case 'S':
 			return Color.LIGHT_GRAY;
-		case 'I':{
-			//BROWN
+		case 'I': {
+			// BROWN
 			return Color.getHSBColor(0.05f, 1.0f, 0.65f);
 		}
 		case 'G':
+
 			return Color.PINK;
 		case 'g':
+
 			return Color.PINK;
 		case 'O':
 			return Color.RED;
@@ -53,14 +60,73 @@ public class GPanel extends JPanel {
 		}
 	}
 
+	private BufferedImage getImage(char c) {
+		BufferedImage image = null;
+		
+		switch (c) {
+		case 'X':
+			try {
+				image = ImageIO.read(new File("imgs/block_02.png"));
+
+			} catch (IOException e) {
+				return null;
+			}
+			return image;
+			
+		case 'H':
+			try {
+				image = ImageIO.read(new File("imgs/hero.png"));
+
+			} catch (IOException e) {
+				return null;
+			}
+			return image;
+			
+		case 'G':
+			try {
+				image = ImageIO.read(new File("imgs/guardian.png"));
+
+			} catch (IOException e) {
+				return null;
+			}
+			return image;
+		case 'g':
+			try {
+				image = ImageIO.read(new File("imgs/sleepingguardian.png"));
+
+			} catch (IOException e) {
+				return null;
+			}
+			return image;
+		
+		 case 'O': 
+			 try {
+					image = ImageIO.read(new File("imgs/ogre.png"));
+
+				} catch (IOException e) {
+					return null;
+				}
+				return image;
+		 
+		default:
+			return null;
+		}
+	}
+
 	@Override
 	public void paintComponent(Graphics g) {
+		BufferedImage image = null;
 		if (map == null)
 			return;
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[i].length; j++) {
-				g.setColor(getColor(map[i][j]));
-				g.fillRect(j * pixelWidth, i * pixelHeight, pixelWidth, pixelHeight);
+				if (map[i][j] == 'G' || map[i][j] == 'g' || map[i][j] == 'X'|| map[i][j] == 'O' || map[i][j] == 'H') {
+					image = getImage(map[i][j]);
+					g.drawImage(image, j * pixelWidth, i * pixelHeight, pixelWidth, pixelHeight, null);
+				} else {
+					g.setColor(getColor(map[i][j]));
+					g.fillRect(j * pixelWidth, i * pixelHeight, pixelWidth, pixelHeight);
+				}
 			}
 		}
 	}
