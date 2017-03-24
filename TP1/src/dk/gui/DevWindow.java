612c8cf0b37221fbce4bca7f9e5de85fa2d89413
Rendoir <Rendoir@172.30.7.39>
2017-03-24 16:19:08 +0000

    Divided into smaller functions

--- TP1/src/dk/gui/DevWindow.java
+++ TP1/src/dk/gui/DevWindow.java
@@ -24,16 +24,8 @@ public class DevWindow {
 	private JFrame devFrame;
 	private GPanel graphics;
 	private JTextField mapX, mapY;
-	private JButton startDev;
-	private JButton btnHero;
-	private JButton btnDoor;
-	private JButton btnOgre;
-	private JButton btnGuardian;
-	private JButton btnWall;
-	private JButton btnRock;
-	private JButton btnKey;
-	private JButton btnFinished;
-	private JButton btnBack;
+	private JButton startDev, btnFinished, btnBack;
+	private JButton btnHero, btnDoor, btnOgre, btnGuardian, btnWall, btnRock, btnKey;
 	private Level level;
 	private ArrayList<Level> levels;
 	private String element;
@@ -46,120 +38,105 @@ public class DevWindow {
 	}
 
 	private void handleAddition(int cellX, int cellY) {
-		if(element == null)
+		if (element == null)
 			return;
 		switch (element) {
-		case "Hero": {
+		case "Hero":
 			Hero hero = new Hero(cellX, cellY);
 			if (level.canAdd(hero))
 				level.setHero(hero);
 			break;
-		}
-		case "Ogre": {
+		case "Ogre":
 			Ogre ogre = new Ogre(cellX, cellY);
 			if (level.canAdd(ogre))
 				level.addOgre(ogre);
 			break;
-		}
-		case "Guardian": {
+		case "Guardian":
 			Guardian rookieG = new RookieG(cellX, cellY);
 			if (level.canAdd(rookieG))
 				level.addGuardian(rookieG);
 			break;
-		}
-		case "Door": {
+		case "Door":
 			Door door = new Door(cellX, cellY);
 			if (level.canAddElement(cellX, cellY))
 				level.addDoor(door);
 			break;
-		}
-		case "Key": {
+		case "Key":
 			Coordinates key = new Coordinates(cellX, cellY);
 			if (level.canAddElement(cellX, cellY))
 				level.setKey(key);
 			break;
-		}
-		case "Wall": {
+		case "Wall":
 			if (level.canAddElement(cellX, cellY))
 				level.addElement('X', cellX, cellY);
 			break;
-		}
-		case "Rock": {
+		case "Rock":
 			if (level.canAddElement(cellX, cellY))
 				level.addElement('I', cellX, cellY);
 			break;
-		}
 		default:
 			return;
 		}
 	}
 
 	private void handleDeletion(int cellX, int cellY) {
-		if(element == null)
+		if (element == null)
 			return;
 		switch (element) {
-		case "Hero": {
+		case "Hero":
 			level.removeHero();
 			break;
-		}
-		case "Ogre": {
+		case "Ogre":
 			Ogre ogre = new Ogre(cellX, cellY);
 			level.removeOgre(ogre);
 			break;
-		}
-		case "Guardian": {
+		case "Guardian":
 			Guardian rookieG = new RookieG(cellX, cellY);
 			level.removeGuardian(rookieG);
 			break;
-		}
-		case "Door": {
+		case "Door":
 			Door door = new Door(cellX, cellY);
 			level.removeDoor(door);
 			break;
-		}
-		case "Key": {
+		case "Key":
 			level.removeKey();
 			break;
-		}
 		case "Wall":
-		case "Rock":{
+		case "Rock":
 			level.removeElement(cellX, cellY);
 			break;
-		}
 		default:
 			return;
 		}
 	}
 
-	private void resetLevel(){
+	private void resetLevel() {
 		level = new Level();
-		try{
+		try {
 			int mapHeight = Integer.parseInt(mapY.getText());
 			int mapWidth = Integer.parseInt(mapX.getText());
 			char defaultMap[][] = graphics.setDefaultMap(mapWidth, mapHeight);
 			level.setMap(defaultMap);
-		} catch(NumberFormatException e) { return; }
+		} catch (NumberFormatException e) {
+			return;
+		}
 		graphics.repaint();
 		graphics.revalidate();
 		graphics.removeMouseListener(mouseListener);
 		graphics.addMouseListener(mouseListener);
 		graphics.requestFocusInWindow();
 	}
-	
-	private void init() {
-		// TODO SETBOUNDS
-		// TODO LABELS
 
-		// Frame
+	private void initFrame() {
 		devFrame = new JFrame();
 		devFrame.setBounds(0, 0, 600, 425);
 		devFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
-		// Graphics Pane
+	private void initGraphics() {
 		graphics = new GPanel(20, 80, 250, 250);
 		graphics.setFocusable(true);
 		mouseListener = new MouseListener() {
-
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				int cellX = graphics.getCellX(e.getX());
@@ -176,98 +153,61 @@ public class DevWindow {
 				graphics.revalidate();
 				graphics.requestFocusInWindow();
 			}
-
 			@Override
-			public void mouseEntered(MouseEvent e) {
-
-			}
-
+			public void mouseEntered(MouseEvent e) {}
 			@Override
-			public void mouseExited(MouseEvent e) {
-
-			}
-
+			public void mouseExited(MouseEvent e) {}
 			@Override
-			public void mousePressed(MouseEvent e) {
-
-			}
-
+			public void mousePressed(MouseEvent e) {}
 			@Override
-			public void mouseReleased(MouseEvent e) {
+			public void mouseReleased(MouseEvent e) {}};
+	}
 
-			}
-
-		};
-
-		// Dimensions
-		mapX = new JTextField(2);
-		mapX.setBounds(0, 0, 30, 20);
-		mapY = new JTextField(2);
-		mapY.setBounds(60, 0, 30, 20);
-
-		// Buttons
-		startDev = new JButton("New Level");
-		startDev.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				resetLevel();
-			}
-		});
-		startDev.setBounds(385, 25, 100, 25);
-
-		btnHero = new JButton("Hero");
-		btnHero.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				element = new String(btnHero.getText());
-				graphics.requestFocusInWindow();
-			}
-		});
-		btnHero.setBounds(385, 50, 100, 25);
-
-		btnDoor = new JButton("Door");
-		btnDoor.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				element = new String(btnDoor.getText());
-				graphics.requestFocusInWindow();
+	public void initDimensions(){
+			mapX = new JTextField(2);
+			mapX.setBounds(0, 0, 30, 20);
+			mapY = new JTextField(2);
+			mapY.setBounds(60, 0, 30, 20);
+	}
+	
+	
+	private void initButtons(){
+		initDevButton();
+		initHeroButton();
+		initDoorButton();
+		initOgreButton();
+		initGuardianButton();
+		initWallButton();
+		initRockButton();
+		initKeyButton();
+		initFinishedButton();
+		initBackButton();
+	}
+	
+	private void initBackButton() {
+		btnBack = new JButton("Back");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				disable();
 			}
 		});
-		btnDoor.setBounds(385, 75, 100, 25);
+		btnBack.setBounds(385, 300, 100, 25);		
+	}
 
-		btnOgre = new JButton("Ogre");
-		btnOgre.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				element = new String(btnOgre.getText());
-				graphics.requestFocusInWindow();
+	private void initFinishedButton() {
+		btnFinished = new JButton("Finished Editting");
+		btnFinished.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (level.isValid()) {
+					levels.add(level);
+					resetLevel();
+				}
 			}
 		});
-		btnOgre.setBounds(385, 100, 100, 25);
+		btnFinished.setBounds(385, 275, 100, 25);		
+	}
 
-		btnGuardian = new JButton("Guardian");
-		btnGuardian.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				element = new String(btnGuardian.getText());
-				graphics.requestFocusInWindow();
-			}
-		});
-		btnGuardian.setBounds(385, 175, 100, 25);
-
-		btnWall = new JButton("Wall");
-		btnWall.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				element = new String(btnWall.getText());
-				graphics.requestFocusInWindow();
-			}
-		});
-		btnWall.setBounds(385, 200, 100, 25);
-
-		btnRock = new JButton("Rock");
-		btnRock.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				element = new String(btnRock.getText());
-				graphics.requestFocusInWindow();
-			}
-		});
-		btnRock.setBounds(385, 225, 100, 25);
-
+	private void initKeyButton() {
 		btnKey = new JButton("Key");
 		btnKey.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
@@ -275,30 +215,96 @@ public class DevWindow {
 				graphics.requestFocusInWindow();
 			}
 		});
-		btnKey.setBounds(385, 250, 100, 25);
+		btnKey.setBounds(385, 250, 100, 25);		
+	}
 
-		btnFinished = new JButton("Finished Editting");
-		btnFinished.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(level.isValid()){
-					levels.add(level);
-					resetLevel();
-				}
+	private void initRockButton() {
+		btnRock = new JButton("Rock");
+		btnRock.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				element = new String(btnRock.getText());
+				graphics.requestFocusInWindow();
 			}
 		});
-		btnFinished.setBounds(385, 275, 100, 25);
+		btnRock.setBounds(385, 225, 100, 25);		
+	}
 
-		
-		btnBack = new JButton("Back");
-		btnBack.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				disable();
+	private void initWallButton() {
+		btnWall = new JButton("Wall");
+		btnWall.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				element = new String(btnWall.getText());
+				graphics.requestFocusInWindow();
 			}
 		});
-		btnBack.setBounds(385, 300, 100, 25);
-		
-		
-		// Add elements to frame
+		btnWall.setBounds(385, 200, 100, 25);		
+	}
+
+	private void initGuardianButton() {
+		btnGuardian = new JButton("Guardian");
+		btnGuardian.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				element = new String(btnGuardian.getText());
+				graphics.requestFocusInWindow();
+			}
+		});
+		btnGuardian.setBounds(385, 175, 100, 25);		
+	}
+
+	private void initOgreButton() {
+		btnOgre = new JButton("Ogre");
+		btnOgre.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				element = new String(btnOgre.getText());
+				graphics.requestFocusInWindow();
+			}
+		});
+		btnOgre.setBounds(385, 100, 100, 25);		
+	}
+
+	private void initDoorButton() {
+		btnDoor = new JButton("Door");
+		btnDoor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				element = new String(btnDoor.getText());
+				graphics.requestFocusInWindow();
+			}
+		});
+		btnDoor.setBounds(385, 75, 100, 25);		
+	}
+
+	private void initHeroButton() {
+		btnHero = new JButton("Hero");
+		btnHero.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				element = new String(btnHero.getText());
+				graphics.requestFocusInWindow();
+			}
+		});
+		btnHero.setBounds(385, 50, 100, 25);		
+	}
+
+	private void initDevButton() {
+		startDev = new JButton("New Level");
+		startDev.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				resetLevel();
+			}
+		});
+		startDev.setBounds(385, 25, 100, 25);
+	}
+
+	private void init() {
+		// TODO SETBOUNDS
+		// TODO LABELS
+		initFrame();
+		initGraphics();
+		initDimensions();
+		initButtons();
+		addElementsToFrame();
+	}
+	
+	public void addElementsToFrame(){
 		devFrame.setResizable(false);
 		devFrame.getContentPane().setLayout(null);
 		devFrame.getContentPane().add(mapX);
@@ -315,7 +321,7 @@ public class DevWindow {
 		devFrame.getContentPane().add(btnFinished);
 		devFrame.getContentPane().add(btnBack);
 	}
-
+	
 	public void enable() {
 		devFrame.setVisible(true);
 	}
@@ -324,8 +330,8 @@ public class DevWindow {
 		devFrame.setVisible(false);
 		gui.run(GUI.Window.Main);
 	}
-	
-	public ArrayList<Level> getLevels(){
+
+	public ArrayList<Level> getLevels() {
 		return levels;
 	}
 }
